c182fc2a8b24b98216432f6fa3107d1521253ca8
ali nouri <alli.nouri.83@gmail.com>
2025-03-28 17:52:47 +0330

    configurations at March 28

--- lua/ali/lazy/init.lua
+++ lua/ali/lazy/init.lua
@@ -1,15 +1,7 @@
 return {
-    "nvim-lua/plenary.nvim",
+	"nvim-lua/plenary.nvim",
 
-    "ThePrimeagen/vim-be-good",
-    "eandrju/cellular-automaton.nvim",
-    "gpanders/editorconfig.nvim",
+	"ThePrimeagen/vim-be-good",
+	"eandrju/cellular-automaton.nvim",
+	"gpanders/editorconfig.nvim",
 }
-
-
--- return {
--- 	"olrtg/nvim-emmet",
--- 	config = function()
--- 		vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
--- 	end,
--- }
